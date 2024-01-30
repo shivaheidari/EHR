a4b265ed6023a798ba4a7391371d1ea465aba8b2
Shiva Heydari <shivaheydari.es@gmail.com>
2024-01-29 20:15:12 -0700

    second

--- Api_flask/1_hello_world.py
+++ Api_flask/1_hello_world.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi all!'
+    return 'Hi Jeferson!'
+
 
 if __name__ == '__main__':
     app.run()
