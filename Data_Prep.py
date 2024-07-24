f0413a1b33065d5c87209cff2d56cdd8c87443c8
Shiva Heydari <shivaheydari.es@gmail.com>
2024-07-24 11:16:51 -0600

    DATA prep

--- Data_Prep.py
+++ Data_Prep.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import URL
 from sqlalchemy import text
+import pandas as pd
 #hostname: 127.0.0.1
 #port = 3306
 #username: root
@@ -8,12 +9,23 @@ from sqlalchemy import text
 engine = create_engine("mysql+pymysql://root:root@localhost:3306/MIMIC")
 
 
+#MIMIC Data admissions
 
 
 with engine.connect() as conn:
-    query = text ("SELECT count(*) FROM MIMIC.d_icd_diagnoses where SHORT_TITLE like '%dementia%';")
+    query = text ("SELECT MIMIC.ehrs.ROW_ID, MIMIC.ehrs.SUBJECT_ID, MIMIC.ehrs.HADM_ID, MIMIC.ehrs.CHARTDATE, MIMIC.ehrs.STORETIME, MIMIC.ehrs.CATEGORY, MIMIC.ehrs.DESCRIPTION, MIMIC.ehrs.CGID, MIMIC.ehrs.ISERROR, MIMIC.ehrs.TEXT, MIMIC.diagnoses_icd.SUBJECT_ID from MIMIC.ehrs,MIMIC.diagnoses_icd WhERE MIMIC.diagnoses_icd.SUBJECT_ID = MIMIC.ehrs.SUBJECT_ID")
     result = conn.execute(query)
     count = result.fetchone()[0]
-    print("number of demenita diagnoses: ", count)
+    print("results", count)
 
-#select ICDcodes 290.0 to 
+#select patinets with dementia
+
+
+#read all the chunks and add to database 
+
+# path ="../Data_chunks/"
+# for i in range(99,100):
+#     chunk = pd.read_csv(path+"chunk"+str(i)+".csv")
+#     chunk = chunk.drop(chunk.columns[0], axis=1)
+#     chunk.to_sql('ehrs', con=engine, if_exists='append', index=False)
+#     print("chunk",i," added to database")
